85721710c92af0f4fc5848cc9ecbdc7638f87d9f
iury <iuryroberto@gmail.com>
2024-01-07 10:31:10 -0300

    Added shebang and some comments

--- hello.py
+++ hello.py
@@ -1,1 +1,7 @@
-print("Hello, World!")
+#!/usr/bin/env python3
+# a linha acima garante que variáveis de ambiente estarão disponíveis ao 
+# programa executado pelo interpretador
+
+# Comentários são marcados com #
+
+print("Hello, World!") #comentários podem estar no meio da linha
